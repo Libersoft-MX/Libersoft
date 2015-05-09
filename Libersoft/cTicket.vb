eec1f736f5af25cd72e08ae390088ea1dd8794bc
Daniel Pérez Cortéz <pcd_mac-000100111@hotmail.com>
2015-05-08 23:46:53 -0500

    Configuraciones iniciales

---
+++ Libersoft/cTicket.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Drawing
+Public Class cTicket
+    Private _Empresa As String
+    Private _Calle As String
+    Private _Colonia As String
+    Private _Municipio As String
+    Private _Logo As Image
+
+End Class
