cd636a1e198712c6221460a4bf3bfd2291c0ec2e
Daniel Pérez Cortéz <pcd_mac-000100111@hotmail.com>
2015-05-09 01:03:00 -0500

    Reg001

---
+++ Libersoft/cBarCode.vb
@@ -0,0 +1,3 @@
+﻿Public Class cBarCode
+
+End Class
--- Libersoft/cTicket.vb
+++ Libersoft/cTicket.vb
@@ -1,9 +1,13 @@
 ﻿Imports System.Drawing
+Imports iTextSharp
 Public Class cTicket
     Private _Empresa As String
     Private _Calle As String
     Private _Colonia As String
     Private _Municipio As String
     Private _Logo As Image
+    Private _Telefono As String
+    Private _BarCode As String
+
 
 End Class
